f5d28eb286443d4323cf72b4f304e8e7696ca67f
UROUS3814 <urous3814@gmail.com>
2023-06-01 18:57:07 +0900

    Update TrainTrace.py

--- NODESERVER/TrainTrace.py
+++ NODESERVER/TrainTrace.py
@@ -20,21 +20,39 @@ def MU_GetJson(apiName, keyword):
     api_key = "637862555675726f34354b695a4d54"
     url = f"http://swopenapi.seoul.go.kr/api/subway/{api_key}/json/{apiName}/0/999/{keyword}"
     response = requests.get(url, headers={"Content-Type": "application/json; charset=utf-8", "Authorization": api_key})
-    output = trainData(json.loads(response.text)["realtimeArrivalList"], len(json.loads(response.text)["realtimeArrivalList"])) 
-    return output   
+    try:
+        
+        if(json.loads(response.text)['errorMessage']['status']==200):
+            output = trainData(json.loads(response.text)['realtimeArrivalList'], len(json.loads(response.text)['realtimeArrivalList'])) 
+            return output   
+        else:
+            print("해당 데이터가 없습니다. 요청 실패")
+    except:
+        print("해당 데이터가 없습니다. 요청 실패")
+        return 0
+
 
 def GTN_GetTrainNum(subwayId, updnLine, station):  #열차 차량번호 얻기, 필요인자: 호선정보, 상하행선 정보, 지하철역
     DATA = MU_GetJson("realtimeStationArrival", station)
-    for i in range (DATA.dataLength):
-        if(DATA.jsonData[i]["subwayId"] == subwayId and DATA.jsonData[i]["updnLine"] == updnLine):
-            return DATA.jsonData[i]["btrainNo"]
+    if(DATA!=0):
+        for i in range (DATA.dataLength):
+            if(DATA.jsonData[i]["subwayId"] == subwayId and DATA.jsonData[i]["updnLine"] == updnLine):
+                return DATA.jsonData[i]["btrainNo"]
+    else:
+        output = trainLocation(0, '잘못된 요청')  
+        return output
+
         
 def GLT_GetArrivalTime(destination, trainNum):  #도착 예정 시간 확인, 필요인자: 목적지, 리턴값: 남은 시간, 
     DATA = MU_GetJson("realtimeStationArrival", destination)
-    for i in range (DATA.dataLength):
-        if(DATA.jsonData[i]["btrainNo"] == trainNum):           
-            output = trainLocation(DATA.jsonData[i]["barvlDt"], DATA.jsonData[i]["arvlMsg3"])  
-            return output
+    if(DATA!=0):
+        for i in range (DATA.dataLength):
+            if(DATA.jsonData[i]["btrainNo"] == trainNum):           
+                output = trainLocation(DATA.jsonData[i]["barvlDt"], DATA.jsonData[i]["arvlMsg3"])  
+                return output
+    else:
+        output = trainLocation(0, '잘못된 요청')  
+        return output
         
 if __name__ == '__main__':
     subwayLineNum = sys.argv[1]
@@ -48,22 +66,28 @@ if __name__ == '__main__':
         # print("남은 시간: ",int(timeTest1.lastTime)//60, "분 ", int(timeTest1.lastTime)%60,"초", ";현재 위치: ", timeTest1.nowLocation,";", end='')
         timeTest2 = GLT_GetArrivalTime(destinationStation, subwayNumber)
         # print("남은 시간: ",int(timeTest2.lastTime)//60, "분 ", int(timeTest2.lastTime)%60,"초", ";현재 위치: ", timeTest2.nowLocation, end='')
-        if(type(timeTest2)=='NoneType'):
+        if(timeTest2 is None):
             timeTest2.lastTime = 0
             print(0,";",0,";",timeTest2.nowLocation, end=';')
-        elif(type(timeTest1)=='NoneType'):
+        elif(timeTest1 is None):
             timeTest1.lastTime = 0
             print(0,";",timeTest2.lastTime,";",timeTest2.nowLocation, end=';')
+        elif(timeTest2.nowLocation == "잘못된 요청"):
+            print(0,";",0,";","요청이 잘못됨", end=';')
         else:
             print(timeTest1.lastTime,";",timeTest2.lastTime,";",timeTest2.nowLocation, end=';')
     else:
         timeTest1 = GLT_GetArrivalTime(startStation, subwayNumber)
         timeTest2 = GLT_GetArrivalTime(destinationStation, subwayNumber)
-        if(type(timeTest1)=='NoneType'):
-            print(0,";",timeTest2.lastTime,";",timeTest2.nowLocation, end=';')
-        elif(type(timeTest2)=='NoneType'):
+        if(timeTest2 is None):
             print(0,";",0,";","요청이 잘못됨", end=';')
+        elif(timeTest1 is None):
+            print(0,";",timeTest2.lastTime,";",timeTest2.nowLocation, end=';')
+        
         # print("남은 시간: ",int(timeTest2.lastTime)//60, "분 ", int(timeTest2.lastTime)%60,"초", ";현재 위치: ", timeTest2.nowLocation, end='')
+        elif(timeTest2.nowLocation == "잘못된 요청"):
+            print(0,";",0,";","요청이 잘못됨", end=';')
         else:
             print(timeTest1.lastTime,";",timeTest2.lastTime,";",timeTest2.nowLocation, end=';')
     print(subwayLineNum,";",startStation,";",destinationStation,";",subwayNumber, end='')
+
